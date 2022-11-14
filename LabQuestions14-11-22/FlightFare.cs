a03f75e28de0e71666e5df6450eef1bad1859140
anunadh.rajesh <anunadhrajesh777@gmail.com>
2022-11-14 21:04:49 +0530

    Add project files.

---
+++ LabQuestions14-11-22/FlightFare.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabQuestions14_11_22
+{
+        internal class FlightFares
+        {
+            private float _fare;
+
+            public float FlightFare
+            {
+                get { return _fare; }
+                set
+                {
+                    float temp = Convert.ToInt32(_bookingTime);
+                    if (temp >= 6 && temp < 9)
+                    {
+                        temp = 10;
+                    }
+                    else if (temp >= 9 && temp < 17)
+                    {
+                        temp = 20;
+                    }
+                    else if (temp >= 17 && temp < 23)
+                    {
+                        temp = 7;
+                    }
+                    else
+                    {
+                        temp = 5;
+                    }
+                    _fare = value;
+                    _fare = _fare + (_fare * (float)(temp / 100));
+                    Console.WriteLine($"Increase in Fare Price {temp}% Booking Fare is : {_fare} ");
+                }
+            }
+
+            private string _bookingTime;
+
+            public string BookingTime
+            {
+                get { return _bookingTime; }
+                set
+                {
+                    _bookingTime = value;
+                    string[] time = value.Split(' ');
+                    if (time[1] == "PM")
+                    {
+                        int _time = Convert.ToInt32(time[0]);
+                        _time += 12;
+                        _bookingTime = Convert.ToString(_time);
+                        Console.WriteLine(_time);
+                        return;
+                    }
+                    else if (time[0] == "12")
+                    {
+                        int _time = Convert.ToInt32(time[0]);
+                        _time = 0;
+                        _bookingTime = Convert.ToString(_time);
+                        return;
+                    }
+                    _bookingTime = time[0];
+
+                }
+            }
+
+
+        }
+    }
---
+++ LabQuestions14-11-22/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LabQuestions14_11_22;
+
+/// Q.) 1. PassengerNames
+
+//string[] arr = new string[10];
+
+//int choice;
+
+//var names = new PassengerNames();
+
+//char flag = 'y';
+//names.EnterData();
+
+
+//while (flag == 'y')
+//{
+//    Console.WriteLine(" 1.Ascending Order \n 2. Descending Order \n 3.Search Names \n 4.Split Names \n 5.Print Duplicates \n 6.Print Length of Names");
+//    Console.Write("\n Enter the choice : ");
+//    choice = int.Parse(Console.ReadLine());
+
+//    switch (choice)
+//    {
+//        case 1:
+//            names.AscendingOrder();
+//            break;
+//        case 2:
+//            names.DescendingOrder();
+//            break;
+//        case 3:
+//            names.SearchNames();
+//            break;
+//        case 4:
+//            names.SplitNames();
+//            break;
+//        case 5:
+//            names.PrintDuplicates();
+//            break;
+//        case 6:
+//            names.PrintLength();
+//            break;
+
+//        default:
+//            Console.WriteLine("Invalid Choice");
+//            break;
+//    }
+
+//    Console.WriteLine("\n Do you want to continue(Y/N)");
+//    flag = char.Parse(Console.ReadLine());
+//}
+
+/// Q.) 2. FlightFares
+
+
+//var f = new FlightFares();
+
+//f.BookingTime = "10 AM";
+//Console.WriteLine(f.BookingTime);
+//f.FlightFare = 100;
+
+///Q.) 4. PassengerDateofBirth
+
+
+//var A = new PDateofBirth();
+//A.SetData();
+//A.GetData();
+//A.Details();
+
+///Q.) 5. FlightEvenOrOdd
+
+var flight = new FlightEvenOdd();
+flight.Assign();
